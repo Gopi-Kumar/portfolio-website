5f914a1665b57f2318e4752a870260561b910189
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-04-20 12:52:11 +0530

    Dropdown added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,34 @@
 const Navbar = ()=>{
-        return(
-            <div className="fixedNavbar navbar">
-                <div className="logo">
-                    <p>gkkaushik</p>
-                </div>
-                <div className="nav">
-                    <a href="#hero">Home</a>
-                    <a href="#skills">Skills</a>
-                    <a href="#projects">Projects</a>
-                    <a href="#message">Hire Me</a>
-                </div>
+    const handleddButton = (e)=>{
+        const ddButton = e.target;
+        const ddMenu = document.querySelector(".nav");
+        if(ddButton.classList[1] === "fa-bars"){
+           ddButton.classList.remove("fa-bars");
+           ddButton.classList.add("fa-close");
+           ddMenu.style.display = "flex";
+
+           
+        }else{
+            ddButton.classList.remove("fa-close");
+            ddButton.classList.add("fa-bars");
+            ddMenu.style.display = "none";
+        }
+    }
+
+    return(
+        <div className="fixedNavbar navbar">
+            <div className="logo">
+                <p>gkkaushik</p>
             </div>
-        )
+            <div className="nav">
+                <a href="#hero">Home</a>
+                <a href="#skills">Skills</a>
+                <a href="#projects">Projects</a>
+                <a href="#message">Hire Me</a>
+            </div>
+            <i className="fa fa-bars" onClick={(e)=>handleddButton(e)}></i>
+        </div>
+    )
 }
 
 export default Navbar;
