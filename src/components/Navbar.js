65c2a9ab5f0b2d1debb16aedaddfd8fd8d347f17
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-04-25 00:26:31 +0530

    warnings fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,26 @@
 const Navbar = ()=>{
-    const handleddButton = (e)=>{
-        const ddButton = e.target;
-        const ddMenu = document.querySelector(".nav");
-        if(ddButton.classList[1] === "fa-bars"){
-           ddButton.classList.remove("fa-bars");
-           ddButton.classList.add("fa-close");
-           ddMenu.style.display = "flex";
+    // const handleddButton = (e)=>{
+    //     const ddButton = e.target;
+    //     const ddMenu = document.querySelector(".nav");
+    //     if(ddButton.classList[1] === "fa-bars"){
+    //        ddButton.classList.remove("fa-bars");
+    //        ddButton.classList.add("fa-close");
+    //        ddMenu.style.display = "flex";
 
            
-        }else{
-            ddButton.classList.remove("fa-close");
-            ddButton.classList.add("fa-bars");
-            ddMenu.style.display = "none";
-        }
-    }
+    //     }else{
+    //         ddButton.classList.remove("fa-close");
+    //         ddButton.classList.add("fa-bars");
+    //         ddMenu.style.display = "none";
+    //     }
+    // }
 
     return(
         <div className="navbar-section">
             <div className="logo"><p>gkkaushik</p></div>
             <div className="navbar">
                 <div className="items">
-                        <a href="#" className="navbar-item">Home</a>
+                        <a href="#skills" className="navbar-item">Home</a>
                         <a href="#skills" className="navbar-item">Skills</a>
                         <a href="#projects" className="navbar-item">Projects</a>
                         <a href="#message" className="navbar-item">Message me</a>
