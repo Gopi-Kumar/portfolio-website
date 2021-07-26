fc2858c3d90e405c56b8fbd0c0f906d07d7f11e4
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-07-26 22:51:53 +0530

    connected to letter box api

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,27 +1,52 @@
+import React, { useState } from 'react';
 function Message() {
-    const handleSubmit = ()=>{
-        console.log("Message send");
+    // let endpoint = "http://localhost:3002";
+    let endpoint = "https://letter-box.netlify.com"
+    let [name,setName] = useState("");
+    let [message,setMessage] = useState("");
+    let [email,setEmail] = useState("");
+    const handleSubmit = () => {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+        "name": name,
+        "email": email,
+        "message": message
+        });
+
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        };
+
+        fetch(`${endpoint}/postletter/gopi`, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
     }
     return (
         <div id="message">
             <h1>Message Us</h1>
-            <form onSubmit={handleSubmit}>
+            <div className="message__form">
                 <div className="info">
                     <div className="name">
                         <label htmlFor="input_name">Name: </label>
-                        <input type="text" id="input_name" />
+                        <input onChange = {(e)=>{setName(e.target.value)}} type="text" id="input_name"/>
                     </div>
                     <div className="email">
                         <label htmlFor="input_email">Email:</label>
-                        <input type="email" id="input_email" />
+                        <input onChange = {(e)=>{setEmail(e.target.value)}} type="email" id="input_email" />
                     </div>
                 </div>
                 <div className="message">
                     <label htmlFor="message">Enter your Message: </label>
-                    <textarea id="message" cols="30" rows="10"></textarea>
+                    <textarea onChange = {(e)=>{setMessage(e.target.value)}} id="message" cols="30" rows="10"></textarea>
                 </div>
-                <input  className="button" type="submit" value="Message Us"/>
-            </form>
+                <button  className="button" onClick={handleSubmit} value="Send message">Send Message</button>
+            </div>
         </div>
     )
 }
