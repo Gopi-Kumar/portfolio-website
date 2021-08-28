77ee4eb096a7bd6ac5e24a9321291d94c5978871
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-08-28 15:25:02 +0530

    responsive navbar and home page created

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -3,24 +3,25 @@ const Navbar = ()=>{
     let dropdownOpened = false;
     const dropdown = (e) => {
         let dropdownButton = e.target;
-        let dropdownMenu = dropdownButton.parentNode.nextSibling;
-        console.log();
+        console.log(dropdownButton)
+        let dropdownMenu = dropdownButton.nextSibling;
         if(dropdownOpened){
             dropdownOpened = false;
             dropdownMenu.style.display = "none";
             dropdownButton.classList.add("fa-bars");
             dropdownButton.classList.remove("fa-window-close");
-        }else{
+            return;
+        }
             dropdownOpened = true;
             dropdownMenu.style.display = "flex";
             dropdownButton.classList.remove("fa-bars");
             dropdownButton.classList.add("fa-window-close");
-        }
+        
     }
     return(
         <div className="navbar">
             <h1 className="name">Gopi Kumar Kaushik</h1>
-            <button onClick={(e)=>dropdown(e)}><i className="fas fa-bars"></i></button>
+           <i onClick={(e)=>dropdown(e)} className="fas fa-bars"></i>
             <nav>
                 <li className="active">Home</li>
                 <li>Skills</li>
